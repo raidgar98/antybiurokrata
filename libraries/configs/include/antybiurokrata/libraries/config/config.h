05c538bf3b0fffe31d3d77153060dbd899f62e83
raidgar98 <raidgar98@onet.pl>
2021-04-03 16:44:13 +0200

    added docs and updated .editorconfig

--- libraries/configs/include/antybiurokrata/libraries/config/config.h
+++ libraries/configs/include/antybiurokrata/libraries/config/config.h
@@ -1,42 +1,57 @@
+/**
+ * @file config.h
+ * @author Krzysztof Mochocki (raidgar98@onet.pl)
+ * @brief base classes to handle config files
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #pragma once
 
-class config
+#include <antybiurokrata/types.hpp>
+
+namespace core
 {
-protected:
-	virtual bool dump_config(const char* file_name) const { return false; };
-	virtual bool load_config(const char* file_name) { return false; }
-};
-
-// config data is not deleted, only release!!!
-class configurable
-{
-protected:
-	const config* _config{ nullptr };
-
-	~configurable()
+	class config
 	{
-		_config = nullptr;
-	}
+	protected:
+		virtual bool dump_config(const char *file_name) const { return false; };
+		virtual bool load_config(const char *file_name) { return false; }
+	};
 
-	template<typename T>
-	const T* get_config() const 
+	// config data is not deleted, only release!!!
+	class configurable
 	{
-		require( _config );
-		return dynamic_cast<const T*>(_config);
-	}
+	protected:
+		const config *_config{nullptr};
 
-public:
+		~configurable()
+		{
+			_config = nullptr;
+		}
 
-	void set_config(const config* cfg, const bool)
-	{
-		require( cfg );
-		_config = cfg;
-	}
+		template <typename T>
+		const T *get_config() const
+		{
+			dassert(_config);
+			return dynamic_cast<const T *>(_config);
+		}
 
-	template<typename T>
-	void set_config(const T* cfg)
-	{
-		if( const config* conf = reinterpret_cast<const config*>(cfg) ) set_config(conf, true);
-		else require(false);
-	}
-};
+	public:
+		void set_config(const config *cfg, const bool)
+		{
+			dassert(cfg != nullptr, "cannot be nullptr");
+			_config = cfg;
+		}
+
+		template <typename T>
+		void set_config(const T *cfg)
+		{
+			if (const config *conf = reinterpret_cast<const config *>(cfg))
+				set_config(conf, true);
+			else
+				dassert(false);
+		}
+	};
+}
--- libraries/configs/include/antybiurokrata/types.hpp
+++ libraries/configs/include/antybiurokrata/types.hpp
@@ -1,8 +1,17 @@
+/**
+ * @file types.hpp
+ * @author Krzysztof Mochocki (raidgar98@onet.pl)
+ * @brief Here are stored most basic checks / definitions
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+*/
+
 #pragma once
 
 // Project includes
 #include <patterns/functor.hpp>
-#include <logger/logger.h>
+#include <antybiurokrata/libraries/logger/logger.h>
 
 // STL
 #include <concepts>
@@ -10,86 +19,122 @@
 
 namespace core
 {
-    using str = std::string;
+	/**
+	 * @brief unifies string type in whole project
+	*/
+	using str = std::string;
 
-    namespace operations
-    {
-        using namespace patterns::functors;
+	/**
+	 * @brief contains basic defninitions and tools for throwing exceptions
+	 */
+	namespace exceptions
+	{
+		/**
+		 * @brief verifies is type can be represented with string
+		 * 
+		 * @tparam T type to check
+		 */
+		template <typename T>
+		concept stringizable = requires { std::is_convertible_v<T, str>; };
 
-        // Prints pong in console
-        struct ping_op : public functor< do_nothing_functor_begin >, private Log<ping_op>
-        {
-            using Log<ping_op>::log;
+		/**
+		 * @brief basic exception, as recommended it's derived freom std::exception
+		 * 
+		 * @tparam T provides logger for all child exception
+		 */
+		template <typename T>
+		struct exception_base : public std::exception, Log<exception_base<T>>
+		{
+			const str ___what; /**< stores message */
 
-            virtual void operator()() override
-            {
-                log << "Pong!" << logger::endl;
-            }
-        };
-    }
+			virtual const char *what() const noexcept override
+			{
+				return (log.get_class_name<T>() + ___what).c_str();
+			}
+		};
 
-    namespace exceptions
-    {
-        template<typename T>
-        concept stringizable = requires { std::is_convertible_v<T, str>; };
+		/**
+		 * @brief simplest exception
+		 */
+		struct exception : public exception_base<exception> {};
 
-        template<typename T>
-        struct exception_base : public std::exception, Log< exception_base<T> >
-        {
-            const str ___what;
+		/**
+		 * @brief default exception, that is raised on failed check
+		 */
+		struct assert_exception : public exception_base<assert_exception> {};
 
-            virtual const char* what() const noexcept override
-            {
-                return (log.get_class_name<T>() + ___what).c_str();
-            }
-        };
+		/**
+		 * @brief same as exception, but additionally prints reason to stdout, usefull, if extended log is required
+		 */
+		struct tee_exception : public exception_base<tee_exception>
+		{
+			template <stringizable U>
+			explicit tee_exception(const U &msg) : ___what{msg} { get_logger().error(what()); }
+		};
 
-        struct exception : public exception_base<exception> {};
+		/**
+		 * @brief verifies is given type has minimum requirements for project-wide exceptions
+		 * 
+		 * @tparam T type to check
+		 */
+		template <typename T>
+		concept supported_exception = requires
+		{
+			std::is_base_of_v<std::exception, T>;
+		};
 
-        // while creating, print it to stdout with logger
-        struct tee_exception : public exception_base<tee_exception>, Log<tee_exception>
-        {
-            using Log<tee_exception>::get_logger;
-            template<stringizable U>
-            explicit tee_exception(const U& msg) : ___what{msg} { get_logger().error( what() ); }
-        };
+		/**
+		 * @brief this is just holder for __log_pass
+		 */
+		struct ____require_base
+		{
+		protected:
+			static constexpr bool __log_pass{false}; /** if set to true logs passed checks */
+		};
 
-        template<typename T>
-        concept supported_exception = requires{
-            std::is_base_of_v<std::exception, T>;
-        };
+		/**
+		 * @brief alternative to asertion
+		 * 
+		 * @tparam _ExceptionType exception to throw if check failed
+		 */
+		template <supported_exception _ExceptionType = exception>
+		struct require : Log<require<_ExceptionType>>, ____require_base
+		{
+			/**
+			 * @brief Construct a new require object, which is also checker
+			 * 
+			 * @tparam MsgType deducted type, of message
+			 * @tparam ExceptionArgs variadic type of additional argument that are passed to exception constructor
+			 * @param _check value to be checked
+			 * @param msg message to pass to exception
+			 * @param argv optional exception arguments
+			 */
+			template <typename MsgType, typename... ExceptionArgs>
+			explicit require(const bool _check, const MsgType &msg = "no message provided", ExceptionArgs &&...argv)
+			{
+				if (_check) [[likely]] // be optimist :)
+				{
+					if constexpr (__log_pass)
+						get_logger() << "passed: `" << msg << '`' << logger::endl;
+				}
+				else [[unlikely]]
+				{
+					get_logger().error("Failed on check");
+					get_logger() << msg << logger::endl;
+					get_logger().print_stacktrace();
+					throw _ExceptionType(msg, std::forward<ExceptionArgs>(argv)...);
+				}
+			}
+		};
 
-        struct ____require_base
-        {
-        protected: 
-            static constexpr bool __log_pass{ false };
-        };
+	};
 
-        template<supported_exception _ExceptionType >
-        struct require : Log<require>, ____require_base
-        {
-            using Log<require>::log;
-
-            template<typename MsgType, typename ... ExceptionArgs>
-            require( const bool _check, const MsgType& msg, ExceptionArgs&&... argv)
-            {
-                if (_check) [[likely]] // be optimist :)
-                {
-                    if constexpr (__log_pass) log << "passed: `" << msg << '`' << logger::endl;
-                }
-                else [[unlikely]]
-                {
-                    log.error("Failed on check");
-                    log << msg << logger::endl;
-                    log.print_stacktrace();
-                    throw _ExceptionType( std::forward<ExceptionArgs>(argv)... );
-                }
-            }
-        };
-
-        struct not_handled_object_exception : public exception_base<not_handled_object_exception> {};
-    };
-
-    using operation_t = patterns::functors::functor_collection_t< operations::ping_op /* update this when adding further operations */ >;
-
+	/**
+	 * @brief alias for assertion project-wide [ c(ustom) assert ]
+	 * 
+	 * @tparam Ex_t exception to throw, by default assert_exception
+	 */
+	template<typename Ex_t = typename exceptions::assert_exception>
+	using cassert = require<Ex_t>;
+	using dassert = cassert<>; /** [ d(efault) asssert ] */
 } // namespace core
