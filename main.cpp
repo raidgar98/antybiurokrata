f544a83d5c538df69bb052273e235482dc21f3cf
raidgar98 <raidgar98@onet.pl>
2021-05-07 12:16:31 +0200

    added format file

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <antybiurokrata/libraries/orm/orm.h>
 // #include <antybiurokrata/windows/mainwindow/mainwindow.h>
 
-#include <signal.h>     // ::signal, ::raise
+#include <signal.h>	 // ::signal, ::raise
 #include <boost/stacktrace.hpp>
 
 /*
@@ -20,55 +20,62 @@ curl 'https://api.elsevier.com/content/search/scopus?query=all(LISOK)&apiKey=a4a
 
 */
 
-void my_signal_handler(int signum) {
-    ::signal(signum, SIG_DFL);
+void my_signal_handler(int signum)
+{
+	::signal(signum, SIG_DFL);
 	std::cout << "sigfault in: " << std::this_thread::get_id() << std::endl;
 	global_logger.print_stacktrace();
 	// std::cout << boost::stacktrace::stacktrace() << std::endl;
-    // boost::stacktrace::safe_dump_to("./backtrace.dump");
-    ::raise(SIGABRT);
+	// boost::stacktrace::safe_dump_to("./backtrace.dump");
+	::raise(SIGABRT);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	::signal(SIGSEGV, &my_signal_handler);
 	::signal(SIGABRT, &my_signal_handler);
 
-	std::locale::global( core::plPL() );
+	std::locale::global(core::plPL());
 	// QApplication a(argc, argv);
 	// MainWindow w;
 	// w.show();
 	// for(const auto& x : *ret) x.print();
 
-///////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////
+
+	while(true) {}
 
 	// bgpolsl
 	core::network::bgpolsl_adapter bgadapter{};
 	core::orm::persons_extractor_t bgperson_visitor{};
 	core::orm::publications_extractor_t bgvisitor{bgperson_visitor};
 	auto res = bgadapter.get_person(argv[1], argv[2]);
-	for(auto& x : *res) x.accept(&bgvisitor);
+	for(auto& x: *res) x.accept(&bgvisitor);
 
 	// orcid
 	core::orm::persons_extractor_t orcid_person_visitor{bgperson_visitor};
-	for(auto& x : orcid_person_visitor.persons) x().publictions().clear();
+	for(auto& x: orcid_person_visitor.persons) x().publictions().clear();
 	core::orm::publications_extractor_t ovisitor{orcid_person_visitor};
 	core::network::orcid_adapter oadapter{};
-	for(const auto& person : orcid_person_visitor.persons)
+	for(const auto& person: orcid_person_visitor.persons)
 	{
 		auto ret = oadapter.get_person(person().orcid()());
-		for(auto& x : *ret) x.accept(&ovisitor);
+		for(auto& x: *ret) x.accept(&ovisitor);
 	}
 
 	// scopus
 	core::network::scopus_adapter sadapter{};
 	const auto ret = sadapter.get_person("0000-0001-7667-6697");
 
+	for(const auto& p: bgperson_visitor.persons)
+		global_logger.info() << "[ size: " << p().publictions().size() << " ] " << patterns::serial::pretty_print{p}
+									<< logger::endl;
 
-
-	for(const auto& p : bgperson_visitor.persons) global_logger.info() << "[ size: " << p().publictions().size() << " ] " << patterns::serial::pretty_print{p} << logger::endl;
 	global_logger << "^^^^^^^^^^^^ BGPOLSL ^^^^^^^^^^^^" << logger::endl;
-	for(const auto& p : orcid_person_visitor.persons) global_logger.info() << "[ size: " << p().publictions().size() << " ] " << patterns::serial::pretty_print{p} << logger::endl;
+	for(const auto& p: orcid_person_visitor.persons)
+		global_logger.info() << "[ size: " << p().publictions().size() << " ] " << patterns::serial::pretty_print{p}
+									<< logger::endl;
+
 	global_logger << "^^^^^^^^^^^^^ ORCID ^^^^^^^^^^^^^" << logger::endl;
 	global_logger << "^^^^^^^^^^^^^ SCOPUS ^^^^^^^^^^^^" << logger::endl;
 	return 0;
