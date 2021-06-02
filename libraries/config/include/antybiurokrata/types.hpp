9c108b0689a13e9a655a76e475e3555d2a58fc91
raidgar98 <raidgar98@onet.pl>
2021-06-02 16:22:18 +0200

    added handle for excptions in gui

--- libraries/config/include/antybiurokrata/types.hpp
+++ libraries/config/include/antybiurokrata/types.hpp
@@ -105,21 +105,25 @@ namespace core
 		 * @tparam MsgType message type, ex: core::str, core::u16str
 		 */
 		template<typename MsgType>
-		struct exception
+		struct exception : public std::exception
 		{
 			/** stores message */
-			MsgType _what;
+			str _what;
 
 			exception() = default;
 
-			template<typename Any>
-			exception(const Any& msg) : _what{msg} {}
+			template<typename Any> exception(const Any& msg) : _what{msg} {}
+			exception(const u16str& msg) { _what = get_conversion_engine().to_bytes(msg); }
+			exception(const u16str_v& msg) : exception{ u16str{ msg.data(), msg.size() }  } {}
+			exception(const str& msg) : _what{ msg } {  }
+			exception(const str_v& msg) : _what{ msg } {  }
 
-			// virtual const char* what() const noexcept override { return this->___what.c_str(); }
-			const MsgType& what() const noexcept { return this->_what; }
+			virtual const char* what() const noexcept override { return _what.data(); }
+			str_v what_v() const noexcept { return this->_what; }
+			u16str w_what() const{ return get_conversion_engine().from_bytes(this->_what); }
+			// const MsgType& what() const noexcept { return this->_what; }
 
 			// virtual const str& what() const noexcept { return this->___what; }
-			// virtual str_v what_v() const noexcept { return this->___what; }
 		};
 
 		/**
@@ -131,10 +135,10 @@ namespace core
 		template<typename T, typename MsgType>
 		struct exception_base : public Log<T>, public exception<MsgType>
 		{
-			// using Log<T>::get_logger;
+			using Log<T>::get_logger;
 
 			template<typename Any>
-			explicit exception_base(const Any& any) { this->_what = any; }
+			explicit exception_base(const Any& any) : exception<MsgType>{any} {} // { this->_what = any; }
 		};
 
 		/**
@@ -311,7 +315,7 @@ namespace core
 			 * 
 			 * @param ex any exception
 			 */
-			explicit error_report(const exception<str>& ex) : error_report{ex.what()} {}
+			explicit error_report(const exception<str>& ex) : error_report{ex._what} {}
 
 			/**
 			 * @brief Construct a new error report object
@@ -319,7 +323,7 @@ namespace core
 			 * @param ex any exception
 			 */
 			explicit error_report(const exception<u16str>& ex) :
-				 error_report{core::get_conversion_engine().to_bytes(ex.what())}
+				 error_report{core::get_conversion_engine().to_bytes(ex.w_what())}
 			{
 			}
 
